028dcb6fafefd287d4f076723f57ef4a6c8ce8a7
Ferdinand Pyttel <fpyttel@f-pyttel.de>
2020-07-12 13:37:08 +0200

    add simple README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Teams Webhook API
+
+A simple project to run a MS Teams Webhook.
+
+POST http://127.0.0.1:8070/api/webhook
--- src/main/java/de/fpyttel/teams/webhook/api/WebhookController.java
+++ src/main/java/de/fpyttel/teams/webhook/api/WebhookController.java
@@ -3,12 +3,9 @@ package de.fpyttel.teams.webhook.api;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.fpyttel.teams.webhook.model.From;
-import de.fpyttel.teams.webhook.model.Recipient;
 import de.fpyttel.teams.webhook.model.WebhookRequest;
 import de.fpyttel.teams.webhook.model.WebhookResponse;
 
